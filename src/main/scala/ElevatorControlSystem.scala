9796d1b15d04e57eaac649ede1f701b288bd2bb6
Luis Osorio <yovendielmundo@gmail.com>
2018-02-28 23:05:30 +0400

    setup scala project

---
+++ src/main/scala/ElevatorControlSystem.scala
@@ -0,0 +1,34 @@
+trait ElevatorControlSystem {
+
+  /**
+    * Querying the state of the elevators.
+    * what floor are they on and where they are going
+    * @return
+    */
+  def status(): Seq[(Int, Int, Int)]
+
+
+  /**
+    * Receiving an update about the status of an elevator.
+    * Here we have chosen to represent elevator state as 3 integers:
+    * Elevator ID, Floor Number, Goal Floor Number
+    * @param elevatorId
+    * @param floor
+    * @param goalFloor
+    */
+  def update(elevatorId:Int, floor:Int, goalFloor:Int)
+
+  /**
+    * Receiving a pickup request.
+    * A pickup request is two integers:
+    * Pickup Floor, Direction (negative for down, positive for up)
+    * @param pickupFloor
+    * @param direction
+    */
+  def pickup(pickupFloor:Int, direction:Int)
+
+  /**
+    * Time-stepping the simulation
+    */
+  def step()
+}
