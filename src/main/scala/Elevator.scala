304ff14f6166fad8fa5b69050fa92260d3a39bf0
Luis Osorio <yovendielmundo@gmail.com>
2018-03-02 13:49:23 +0400

    elevator movement implementation and test cases

--- src/main/scala/Elevator.scala
+++ src/main/scala/Elevator.scala
@@ -1,4 +1,4 @@
-import Elevator.{Direction, Id, NoDir, Status}
+import Elevator.{Direction, Id, NoDirection, Status}
 
 object Elevator {
   object Status {
@@ -11,44 +11,83 @@ object Elevator {
   sealed trait Direction
   object Down extends Direction
   object Up extends Direction
-  object NoDir extends Direction
+  object NoDirection extends Direction
 
   def apply(id: Int, maxNumberOfGoals: Int = 3) = new Elevator(Id(id), maxNumberOfGoals)
 }
 
 case class Elevator(id: Id, maxNumberOfGoals: Int) {
   var floor: Int = 0
-  var goalFloor: List[Int] = List()
-  var direction: Direction = NoDir
+  var goalFloors: List[Int] = List()
+  var pickupDirection: Direction = NoDirection
 
   def update(floor: Int, goalFloor: Int) = {
     this.floor = floor
-    this.goalFloor = List(goalFloor)
+    goalFloors = List(goalFloor)
   }
 
-  def status: Status = Status(id, floor, goalFloor)
+  def status: Status = Status(id, floor, goalFloors)
 
-  def pickup(floor:Int, direction: Direction): Unit = {
+  def pickup(goalFloor: Int, direction: Direction): Unit = {
     if (canPickup) {
-      if (this.direction == NoDir) this.direction = direction
-      if (this.direction == direction) goalFloor = goalFloor :+ floor
-    }
+      if (goalFloors.isEmpty) {
+        pickupDirection = direction
+        goalFloors = List(goalFloor)
+      } else {
 
+        if (pickupDirection == direction && isOnTheWay(goalFloor))
+          goalFloors = insert(goalFloor, goalFloors)(currentOrd)
+
+      }
+    }
   }
 
-  def canPickup: Boolean = goalFloor.size < maxNumberOfGoals
+  def canPickup: Boolean = goalFloors.size < maxNumberOfGoals
 
   def step: Unit = {
-    if (!goalFloor.isEmpty) {
-      floor += 1
-    }
+    if (isMoving)
+      floor = nextFloor
+
     updateState
   }
 
+
+  def isAscending: Boolean = goalFloors.head > floor
+
+  def isDescending: Boolean = goalFloors.head < floor
+
+  def isStopped: Boolean = goalFloors.isEmpty
+
+  def isMoving: Boolean = !isStopped
+
+  private def nextFloor: Int = floor + (if (isAscending) 1 else if (isDescending) -1 else 0)
+
+  private def isOnTheWay(goalFloor: Int): Boolean =
+    if (isAscending) goalFloor > floor
+    else goalFloor < floor
+
   private def updateState(): Unit = {
-    if (!goalFloor.isEmpty  && floor == goalFloor.head)
-      goalFloor = goalFloor.tail
+    if (!goalFloors.isEmpty  && floor == goalFloors.head)
+      goalFloors = goalFloors.tail
+
+    if (goalFloors.isEmpty)
+      pickupDirection = NoDirection
   }
 
+  private def currentOrd(): Ordering[Int] =
+    if (isAscending) (x: Int, y: Int) => x - y
+    else (x: Int, y: Int) => y - x
+
+  private def insert(y: Int, ys: List[Int])(implicit ord: Ordering[Int]): List[Int] = ys match {
+    case List() => y :: List()
+    case z :: zs =>
+      if (ord.lt(y, z)) y :: z :: zs
+      else z :: insert(y, zs)
+  }
+
+//  def insert(xs: List[Int], x: Int)(implicit ord: Ordering[Int]): List[Int] = {
+//    val (h, t) = xs.span(ord.lt(_, x))
+//    h ::: (x :: t)
+//  }
 }
 
--- src/test/scala/ElevatorTest.scala
+++ src/test/scala/ElevatorTest.scala
@@ -1,4 +1,4 @@
-import Elevator.{Down, Id, Status, Up}
+import Elevator._
 import org.scalatest.{FunSuite, Matchers}
 
 class ElevatorTest extends FunSuite with Matchers {
@@ -8,7 +8,7 @@ class ElevatorTest extends FunSuite with Matchers {
 
     elevator.id should be(Id(1))
     elevator.floor should be(0)
-    elevator.goalFloor should be(List())
+    elevator.goalFloors should be(List())
   }
 
   test("test get status") {
@@ -23,22 +23,24 @@ class ElevatorTest extends FunSuite with Matchers {
     elevator.update(floor = 1, goalFloor = 5)
 
     elevator.floor should be(1)
-    elevator.goalFloor should be(List(5))
+    elevator.goalFloors should be(List(5))
     elevator.status should be(Status(id = 1, floor = 1, goalFloor = List(5)))
   }
 
   test("test set to pickup some passenger") {
     val elevator = Elevator(id = 1)
 
-    elevator.pickup(floor = 3, direction = Down)
+    elevator.pickup(goalFloor = 3, direction = Down)
+
+    elevator.pickupDirection should be(Down)
     elevator.status should be(Status(1, 0, List(3)))
   }
 
   test("test canPickup is false when the elevator is full") {
     val elevator = Elevator(id = 1, maxNumberOfGoals = 2)
 
-    elevator.pickup(floor = 3, direction = Down)
-    elevator.pickup(floor = 1, direction = Down)
+    elevator.pickup(goalFloor = 3, direction = Down)
+    elevator.pickup(goalFloor = 1, direction = Down)
 
     elevator.canPickup should be(false)
   }
@@ -46,48 +48,103 @@ class ElevatorTest extends FunSuite with Matchers {
   test("test canPickup is true when the elevator is not full") {
     val elevator = Elevator(id = 1, maxNumberOfGoals = 5)
 
-    elevator.pickup(floor = 1, direction = Down)
-    elevator.pickup(floor = 2, direction = Down)
-    elevator.pickup(floor = 3, direction = Down)
-    elevator.pickup(floor = 4, direction = Down)
+    elevator.pickup(goalFloor = 1, direction = Down)
+    elevator.pickup(goalFloor = 2, direction = Down)
+    elevator.pickup(goalFloor = 3, direction = Down)
+    elevator.pickup(goalFloor = 4, direction = Down)
 
     elevator.canPickup should be(true)
   }
 
-  test("test does not pickup passengers in an opposite direction") {
+  test("test doesn't pickup passengers in an opposite direction") {
     val elevator = Elevator(id = 1)
 
-    elevator.pickup(floor = 3, direction = Down)
-    elevator.status should be(Status(1, 0, List(3)))
+    elevator.pickup(goalFloor = 3, direction = Down)
 
-    elevator.pickup(floor = 1, direction = Up)
+    elevator.pickup(goalFloor = 1, direction = Up)
+
+    elevator.pickupDirection should be(Down)
     elevator.status should be(Status(1, 0, List(3)))
   }
 
-  test("test does not pickup passengers when is full") {
+  test("test doesn't pickup passengers when is full") {
     val elevator = Elevator(id = 1, maxNumberOfGoals = 1)
 
-    elevator.pickup(floor = 3, direction = Down)
-    elevator.status should be(Status(1, 0, List(3)))
+    elevator.pickup(goalFloor = 3, direction = Down)
+    // elevator is already full
 
-    elevator.pickup(floor = 1, direction = Down)
+    elevator.pickup(goalFloor = 5, direction = Down)
+
+    elevator.pickupDirection should be(Down)
     elevator.status should be(Status(1, 0, List(3)))
+  }
+
+  test("test doesn't pickup passengers when goal floor is not on the way") {
+    val elevator = Elevator(id = 1)
+
+    elevator.update(floor = 4, goalFloor = 5)
+    elevator.pickupDirection = Down
+    // elevator is in the 4th floor
+
+    elevator.pickup(goalFloor = 3, direction = Down)
+    elevator.status should be(Status(1, 4, List(5)))
   }
 
   test("test set to pickup many passengers in the same direction") {
     val elevator = Elevator(id = 1)
 
-    elevator.pickup(floor = 3, direction = Down)
+    elevator.pickup(goalFloor = 3, direction = Down)
     elevator.status should be(Status(1, 0, List(3)))
 
-    elevator.pickup(floor = 5, direction = Down)
+    elevator.pickup(goalFloor = 5, direction = Down)
     elevator.status should be(Status(1, 0, List(3, 5)))
 
-    elevator.pickup(floor = 6, direction = Down)
+    elevator.pickup(goalFloor = 6, direction = Down)
     elevator.status should be(Status(1, 0, List(3, 5, 6)))
   }
 
-  test("test do useless steps when there is no goals") {
+  test("test goals floors list is ascending when elevator is ascending") {
+    val elevator = Elevator(id = 1, maxNumberOfGoals = 5)
+
+    elevator.pickup(goalFloor = 3, direction = Down)
+    elevator.status should be(Status(1, 0, List(3)))
+    elevator.isAscending should be(true)
+
+    elevator.pickup(goalFloor = 7, direction = Down)
+    elevator.status should be(Status(1, 0, List(3, 7)))
+    elevator.isAscending should be(true)
+
+    elevator.pickup(goalFloor = 6, direction = Down)
+    elevator.status should be(Status(1, 0, List(3, 6, 7)))
+    elevator.isAscending should be(true)
+
+    elevator.pickup(goalFloor = 5, direction = Down)
+    elevator.status should be(Status(1, 0, List(3, 5, 6, 7)))
+    elevator.isAscending should be(true)
+  }
+
+  test("test goals floors list is descending when elevator is descending") {
+    val elevator = Elevator(id = 1, maxNumberOfGoals = 5)
+
+    elevator.update(floor = 10, goalFloor = 0)
+    elevator.pickupDirection = Down
+    elevator.status should be(Status(1, 10, List(0)))
+    elevator.isDescending should be(true)
+
+    elevator.pickup(goalFloor = 3, direction = Down)
+    elevator.status should be(Status(1, 10, List(3, 0)))
+    elevator.isDescending should be(true)
+
+    elevator.pickup(goalFloor = 7, direction = Down)
+    elevator.status should be(Status(1, 10, List(7, 3, 0)))
+    elevator.isDescending should be(true)
+
+    elevator.pickup(goalFloor = 6, direction = Down)
+    elevator.status should be(Status(1, 10, List(7, 6, 3, 0)))
+    elevator.isDescending should be(true)
+  }
+
+  test("test to do steps is useless when there is no goals") {
     val elevator = Elevator(id = 1)
 
     elevator.step
@@ -95,21 +152,118 @@ class ElevatorTest extends FunSuite with Matchers {
     elevator.step
 
     elevator.floor should be(0)
+    elevator.pickupDirection should be(NoDirection)
+    elevator.isStopped should be(true)
     elevator.status should be(Status(1, 0, List()))
   }
 
-  test("test do steps and reach one goal floor") {
+  test("test to do steps ascending and reach a goal floor") {
     val elevator = Elevator(id = 1)
 
-    elevator.pickup(floor = 3, direction = Down)
+    elevator.pickup(goalFloor = 3, direction = Down)
+
     elevator.step
     elevator.floor should be(1)
+
+
     elevator.step
     elevator.floor should be(2)
+
+
     elevator.step
     elevator.floor should be(3)
 
+    elevator.pickupDirection should be(NoDirection)
+    elevator.isStopped should be(true)
     elevator.status should be(Status(1, 3, List()))
   }
+
+  test("test to do steps descending and reach a goal floor") {
+    val elevator = Elevator(id = 1)
+
+    elevator.update(floor = 3, goalFloor = 0)
+    elevator.pickupDirection = Up
+
+    elevator.step
+    elevator.floor should be(2)
+
+    elevator.step
+    elevator.floor should be(1)
+
+    elevator.step
+    elevator.floor should be(0)
+
+    elevator.pickupDirection should be(NoDirection)
+    elevator.isStopped should be(true)
+    elevator.status should be(Status(1, 0, List()))
+  }
+
+  test("test doesn't pickup passengers in an opposite direction when elevator is moving") {
+    val elevator = Elevator(id = 1)
+
+    elevator.pickup(goalFloor = 4, direction = Down)
+    elevator.isAscending should be(true)
+
+    elevator.step
+    elevator.floor should be(1)
+
+    // this pickup request should be ignored
+    elevator.pickup(goalFloor = 3, direction = Up)
+    elevator.status should be(Status(1, 1, List(4)))
+
+    elevator.step
+    elevator.floor should be(2)
+    elevator.status should be(Status(1, 2, List(4)))
+
+    elevator.step
+    elevator.floor should be(3)
+
+    elevator.step
+    elevator.floor should be(4)
+    elevator.pickupDirection should be(NoDirection)
+    elevator.status should be(Status(1, 4, List()))
+  }
+
+
+
+  test("test to do steps picking up many goal floors") {
+    val elevator = Elevator(id = 1)
+
+    elevator.pickup(goalFloor = 4, direction = Down)
+
+    elevator.step
+    elevator.floor should be(1)
+
+    elevator.pickup(goalFloor = 3, direction = Down)
+
+    elevator.step
+    elevator.floor should be(2)
+    elevator.status should be(Status(1, 2, List(3, 4)))
+
+    elevator.step
+    elevator.floor should be(3)
+    elevator.status should be(Status(1, 3, List(4)))
+    elevator.isMoving should be(true)
+
+    // pickup should be ignored
+    elevator.pickup(goalFloor = 1, direction = Down)
+    elevator.status should be(Status(1, 3, List(4)))
+
+    // pickup accepted
+    elevator.pickup(goalFloor = 6, direction = Down)
+    elevator.status should be(Status(1, 3, List(4, 6)))
+
+    elevator.step
+    elevator.floor should be(4)
+    elevator.status should be(Status(1, 4, List(6)))
+    elevator.isMoving should be(true)
+
+    elevator.step
+    elevator.step
+    elevator.pickupDirection should be(NoDirection)
+    elevator.isStopped should be(true)
+    elevator.status should be(Status(1, 6, List()))
+  }
+
 
 }
